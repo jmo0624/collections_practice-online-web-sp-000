4c9dd5f023791ccae3a073b0695eac6a34027f07
Josh Morris <jmo0624@gmail.com>
2019-05-05 21:19:28 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -11,7 +11,7 @@ def sort_array_char_count(strings)
 end
 
 def swap_elements(strings)
-  strings[1], stings[2] = strings[2], strings[1]
+  strings[1], strings[2] = strings[2], strings[1]
   return strings
 end
 
