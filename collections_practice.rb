9bd514d07f248403a3bcdda391bf3e08b2abaebe
Josh Morris <jmo0624@gmail.com>
2019-05-05 21:07:50 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -17,3 +17,8 @@ end
 
 def swap_elements_from_to(array, index, destination_index)
   array[index], array[destination_index] = array[destination_index], array[index]
+  return array
+end
+
+def reverse_array(integers)
+  
